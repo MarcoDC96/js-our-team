d43f6d3c3673ef3109a9d0fae42a7e98a1c278e0
MarcoDC96 <marco.decicco@live.it>
2023-11-21 15:56:37 +0100

    finish...

--- js/main.js
+++ js/main.js
@@ -1,45 +1,39 @@
 let ourTeam = 
 [
 {
-    nome: "Wayne",
-    cognome: "Barnett",
+    nome: "Wayne Barnett",
     ruolo: "Founder & CEO",
-    foto: "wayne-barnett-founder-ceo.png"
+    foto: "./img/wayne-barnett-founder-ceo.jpg"
 },
 
 {
-    nome: "Angela",
-    cognome: "Caroll",
+    nome: "Angela Caroll",
     ruolo: "Chief Editor",
-    foto: "angela-caroll-chief-editor.png"
+    foto: "./img/angela-caroll-chief-editor.jpg"
 },
 
 {
-    nome: "Walter",
-    cognome: "Gordon",
+    nome: "Walter Gordon",
     ruolo: "Office Manager",
-    foto: "walter-goldon-office-manager.png"
+    foto: "./img/walter-gordon-office-manager.jpg"
 },
 
 {
-    nome: "Angela",
-    cognome: "Lopez",
+    nome: "Angela Lopez",
     ruolo: "Social Media Manager",
-    foto: "angela-lopez-social-media-manager.png"
+    foto: "./img/angela-lopez-social-media-manager.jpg"
 },
 
 {
-    nome: "Scott",
-    cognome: "Estrada",
+    nome: "Scott Estrada",
     ruolo: "Developer",
-    foto: "scott-estrada-developer.png"
+    foto: "./img/scott-estrada-developer.jpg"
 },
 
 {
-    nome: "Barbara",
-    cognome: "Ramos",
+    nome: "Barbara Ramos",
     ruolo: "Graphic Designer",
-    foto: "barbara-ramos-graphic-designer.png"
+    foto: "./img/barbara-ramos-graphic-designer.jpg"
 }
 ]
 
@@ -47,8 +41,29 @@ console.log(ourTeam);
 
 for(let i = 0; i < ourTeam.length; i++){
     console.log("Nome:", ourTeam[i].nome);
-    console.log("Cognome:", ourTeam[i].cognome);
     console.log("Ruolo:", ourTeam[i].ruolo);
     console.log("foto:", ourTeam[i].foto);
     console.log("");
 }
+
+let containerCard = document.getElementById("card") 
+    for (let i = 0; i < ourTeam.length; i++) { 
+    let member = ourTeam[i]
+    for (let key in member){ 
+        let memberDetails = member[key]
+    }
+
+    
+let card =  `   
+<div class="col-3 m-2"> 
+<div class="card"> <img src="${[member.foto]}" 
+class="card-img-top" alt="foto"> <div class="card-body">   
+<h5 class="titolo-card">${[member.nome]}</h5>   
+<p class="card-text">   ${[member.ruolo]}</p>   
+</div> 
+</div> 
+</div> `
+
+containerCard.insertAdjacentHTML("beforeend",card)
+}
+
